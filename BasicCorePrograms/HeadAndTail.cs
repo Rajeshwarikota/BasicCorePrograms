d0a4957db996e133a0be42262d8f7f7969aa382e
Rajeshwarikota <kotarajeswari003.com>
2023-01-26 14:12:19 +0530

    [Rajeshwari] Adding Head and Tails problem

--- BasicCorePrograms/HeadAndTail.cs
+++ BasicCorePrograms/HeadAndTail.cs
@@ -8,6 +8,27 @@ namespace BasicCorePrograms
 {
      class HeadAndTail
     {
-
+        public static void HeadandTailPercentage(int n)
+        {
+            int headCount=0, tailCount=0;
+            Random random= new Random();
+            for (int i = 0; i < n; i++)
+            {
+                double randomFlip = random.NextDouble();
+                if (randomFlip < 0.5)
+                {
+                    tailCount++;
+                }
+                else 
+                {
+                headCount++;
+                }
+            }
+            Console.WriteLine("Heads= {0} times", headCount);
+            Console.WriteLine("Tails= {0} times", tailCount);
+            double headPercentage = (headCount * 100) / n;
+            double tailPercentage = (tailCount * 100) / n;
+            Console.WriteLine("Head Percentage = {0} Tail Percentage = {1}", headPercentage, tailPercentage);
+        }
     }
-}
+    }
--- BasicCorePrograms/Program.cs
+++ BasicCorePrograms/Program.cs
@@ -11,6 +11,9 @@ namespace BasicCorePrograms
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome To Basic Core Programs");
+            Console.WriteLine("Enter the number of times you want to flip the coin");
+            int n = Convert.ToInt32(Console.ReadLine());
+            HeadAndTail.HeadandTailPercentage(n);
             Console.ReadLine();
         }
     }
