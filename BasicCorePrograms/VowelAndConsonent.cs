88430c6fe94f79fe506c8b72d60fc3f107193c8b
Rajeshwarikota <kotarajeswari003.com>
2023-01-27 14:21:52 +0530

    [Rajeshwari] Adding Vowel Or Consonent program

---
+++ BasicCorePrograms/VowelAndConsonent.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCorePrograms
+{
+    class VowelAndConsonent
+    {
+        public static void VowelRConsonentCheck()
+        {
+            char ch;
+            Console.WriteLine("Enter an Alphabet to Check Vowel Or Not");
+            ch = Convert.ToChar(Console.ReadLine());
+
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') 
+            {
+                Console.WriteLine("Its a Vowel");
+            }
+            else 
+            {
+                Console.WriteLine("Its a Consonent");
+            }
+        }
+    }
+}
