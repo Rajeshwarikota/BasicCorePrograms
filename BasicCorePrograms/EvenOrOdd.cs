306025eacbb9da545cfd82805f9902376e5af1a9
Rajeshwarikota <kotarajeswari003.com>
2023-01-27 13:41:32 +0530

    [Rajeshwari] Adding Even Or Odd program

---
+++ BasicCorePrograms/EvenOrOdd.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCorePrograms
+{
+    class EvenOrOdd
+    {
+        public static void EvenROddCheck()
+        {
+    
+            Console.WriteLine("Enter The Number To Check");
+            int n = Convert.ToInt32(Console.ReadLine());
+            if (n% 2 == 0)
+            {
+               
+                Console.WriteLine("Number is even:"+ n);
+            }
+            else 
+            {
+                Console.WriteLine("Number is Odd:"+ n);
+            }
+        }
+    }
+}
--- BasicCorePrograms/Program.cs
+++ BasicCorePrograms/Program.cs
@@ -11,7 +11,7 @@ namespace BasicCorePrograms
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome To Basic Core Programs");
-            Console.WriteLine("\n1:HeadAndTail\n2:LeapYear\n3:PowerOfTwo\n4:HarmonicNumber\n5:ReaminderandQuotient\n6:SwapTwoNumbers\n11:Exit");
+            Console.WriteLine("\n1:HeadAndTail\n2:LeapYear\n3:PowerOfTwo\n4:HarmonicNumber\n5:ReaminderandQuotient\n6:SwapTwoNumbers\n7:EvenOrOdd\n11:Exit");
             int option = Convert.ToInt32(Console.ReadLine());
 
             switch (option)
@@ -33,6 +33,9 @@ namespace BasicCorePrograms
                     break;
                 case 6:
                     SwappingTwoNumbers.Swapping();
+                    break;
+                case 7:
+                    EvenOrOdd.EvenROddCheck();
                     break;
                 default:
                     Console.WriteLine("choose the above options");
